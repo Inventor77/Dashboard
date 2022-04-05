f192a7eafa8b01d0a8f2f12cd108fc0a4c6718d3
Shreedutt <shreedutt77@gmail.com>
2022-04-05 19:50:19 +0530

    Login form UI is completed

--- src/layout/login/Login.jsx
+++ src/layout/login/Login.jsx
@@ -1,15 +1,20 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import './Login.scss'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 import CustomField from '../../components/CustomField';
+import Rocket from '../../assets/images/rocket.png'
 
 const validationYupSchema = yup.object({
-    name: yup.string().required("").min(2, 'Name is too Short!')
-        .max(70, 'Name is too Long!')
+    name: yup.string().min(2, 'Name is too Short!')
+        .max(36, 'Name is too Long!')
         .required('Name is required field'),
-    // password: 
+    password: yup.string()
+        .min(8, 'Password is too Short!')
+        .max(25, 'Password is too Long!')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])$/gm, "Please Enter Valid Password")
+        .required('Password is required field')
 });
 
 export default class Login extends Component {
@@ -17,50 +22,55 @@ export default class Login extends Component {
     render() {
         return (
             <div className='login_layout flex w-full min-h-screen justify-center items-center'>
-                <div className='form_container bg-cyan-700 flex flex-col md:flex-row md:space-y-0 md:space-x-6 space-y-6 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white'>
-                    <div className='bg-white rounded-xl shadow-lg p-8'>
-                        {/* <h1 className='font-bold text-4xl tracking-wide'>
+                <div className='form_container bg-cyan-700 flex flex-col md:flex-row md:space-y-0 md:space-x-6 space-y-6 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden'>
+                    <div className="relative">
+                        <div className="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -left-20 -top-20"></div>
+                        <div className="absolute z-0 w-40 h-40 bg-teal-400 rounded-full -right-28 -bottom-28"></div>
+                        <div className='relative z-10  bg-white rounded-xl shadow-lg p-8'>
+                            {/* <h1 className='font-bold text-4xl tracking-wide'>
                             Login
                         </h1> */}
-                        <Formik
-                            validationSchema={validationYupSchema}
-                            initialValues={{ name: "", password: "" }}
-                            onSubmit={(val) => console.log(val)}
-                        >
-                            {({ errors, touched }) => (
-                                <Form
-                                    className='flex flex-col space-y-4 text-gray-700 md:w-84'
-                                >
-                                    < CustomField
-                                        label="Name:"
-                                        name="name"
-                                        type="text"
-                                        errors={errors.name}
-                                        touched={touched.name}
-                                    />
-                                    < CustomField
-                                        label="Password:"
-                                        name="password"
-                                        type="password"
-                                        errors={errors.password}
-                                        touched={touched.password}
-                                    />
-                                    {/* <label >Password:</label>
-                                    <Field name='password' type='password' /> */}
-                                    <br></br>
-                                    <button className="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-8 py-2 uppercase text-sm" type="submit">Submit</button>
-                                </Form>
-                            )}
-                        </Formik>
-                    </div>
-
-                    {/* <div className="flex flex-col justify-between">
-                        <div>
-                            <h1 className='font-bold text-4xl tracking-wide'>
-                                Hello, User
-                            </h1>
+                            <Formik
+                                validationSchema={validationYupSchema}
+                                initialValues={{ name: "", password: "" }}
+                                onSubmit={(val) => console.log(val)}
+                            >
+                                {({ errors, touched }) => (
+                                    <Form
+                                        className='flex flex-col justify-between space-y-4 text-gray-600 md:w-80 h-96'
+                                    >
+                                        <div className="text-2xl text-gray-400 font-bold">
+                                            Login
+                                        </div>
+                                        < CustomField
+                                            label="Name:"
+                                            name="name"
+                                            type="text"
+                                            errors={errors.name}
+                                            touched={touched.name}
+                                        />
+                                        < CustomField
+                                            label="Password:"
+                                            name="password"
+                                            type="password"
+                                            errors={errors.password}
+                                            touched={touched.password}
+                                        />
+                                        <button className="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-8 py-2 uppercase text-sm" type="submit">Submit</button>
+                                        <div className="text-sm">
+                                            Not have an account?
+                                            <span className='underline text-teal-500 ml-2 cursor-pointer'>Create your account</span>
+                                        </div>
+                                    </Form>
+                                )}
+                            </Formik>
                         </div>
-                    </div> */}
+                    </div>
+                    <div className="flex flex-col justify-center w-fit ">
+                        <div className='w-80 sm:w-112'>
+                            <img src={Rocket} className='w-full' alt="rocket" />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
